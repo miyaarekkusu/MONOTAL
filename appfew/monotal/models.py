eb4c1a8ab5099b3eb45c715c5122fc186e2b444a
kohta <kohta@cfkobo.com>
2025-11-22 16:46:41 +0900

    商品

--- appfew/monotal/models.py
+++ appfew/monotal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """商品テーブル"""
+    name = models.CharField(max_length=200, verbose_name='商品名')
+    image = models.ImageField(upload_to='products/', verbose_name='商品画像')
+    description = models.TextField(verbose_name='商品詳細')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='商品価格')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日時')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='更新日時')
+
+    class Meta:
+        verbose_name = '商品'
+        verbose_name_plural = '商品'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
